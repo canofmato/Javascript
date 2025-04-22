3acd2d49de4645252db94d82842007dffb5b54b0
canofmato <gunmannduu@gmail.com>
2025-04-22 22:05:16 +0900

    [level 0] Title: 배열의 평균값, Time: 0.17 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = 0;
+    let sum = 0;
+    let count = 0;
+    for (number of numbers) {
+        sum += number;
+        count++;
+    }
+    answer = sum / count;
+    return answer;
+}
